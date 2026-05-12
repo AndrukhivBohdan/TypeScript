169d6f429dce807cdc01ab160436c2a39f3a1deb
AndrukhivBohdan <114477382+AndrukhivBohdan@users.noreply.github.com>
2026-05-12 08:11:52 +0300

    Add files via upload

---
+++ LaboratoryWork2/build/js/app.js
@@ -0,0 +1,28 @@
+import { UIService } from './UIService.js';
+const ui = new UIService('content');
+const $ajax = window.$ajaxUtils;
+document.getElementById('homeLink')?.addEventListener('click', (e) => {
+    e.preventDefault();
+    ui.renderHome();
+});
+document.getElementById('catalogLink')?.addEventListener('click', (e) => {
+    e.preventDefault();
+    loadCategories();
+});
+function loadCategories() {
+    $ajax.sendGetRequest("../build/data/categories.json", (data) => {
+        ui.renderCategories(data, loadCategory, loadRandomCat);
+    });
+}
+function loadCategory(shortname) {
+    $ajax.sendGetRequest(`../build/data/categories/${shortname}.json`, (data) => {
+        ui.renderItems(data);
+    });
+}
+function loadRandomCat() {
+    $ajax.sendGetRequest("../build/data/categories.json", (categories) => {
+        const random = categories[Math.floor(Math.random() * categories.length)];
+        loadCategory(random.shortname);
+    });
+}
+ui.renderHome();
